88d484d24b35bb889dd1954c396935616004a288
Nitis <reberrus@gmail.com>
2022-09-18 15:05:54 +0700

    Test2

--- totalPages.js
+++ totalPages.js
@@ -4,7 +4,7 @@ function totalPages(arrayItems, rowsPerPage) {
   if (arrayItems == null || arrayItems == undefined) {
     return undefined
   }
-  if (rowsPerPage== null || rowsPerPage == undefined) {
+  if (rowsPerPage == null || rowsPerPage == undefined || rowsPerPage == 0) {
     return 1
   }  
   else {
